1f87adda918e6cb620bbe3e5bf3c94cc6d5d2e4d
Mahir <m.mersimoski1999@gmail.com>
2021-09-01 13:44:43 +0200

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,39 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      _id: "2",
+      name: "Slim Fit Pants",
+      category: "Pants",
+      image: "/images/p6.jpg",
+      price: 220,
+      brand: "Adidas",
+      rating: 3,
+      numReviews: 8,
+      description: "high quality pants",
+    },
+    {
+      _id: "3",
+      name: "Lacoste Hat",
+      category: "Hats",
+      image: "/images/p2.jpg",
+      price: 30,
+      brand: "Lacoste",
+      rating: 5,
+      numReviews: 15,
+      description: "high quality product",
+    },
+  ],
+};
+
+export default data;
